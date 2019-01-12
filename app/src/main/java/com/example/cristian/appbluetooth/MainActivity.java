e9083c92f9a676284a53af3b5871f5902812a46b
Unknown <mamanicristian92@gmail.com>
2019-01-12 17:15:19 -0300

    Primera Pantalla (no funcional)
    pantalla en xml no testeada no funcional

---
+++ app/src/main/java/com/example/cristian/appbluetooth/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cristian.appbluetooth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import static com.example.cristian.appbluetooth.R.layout.activity_main;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
+    }
+}
