03fcd1f37e7b607bc1308dae95d92185f95f01ac
Unknown <mamanicristian92@gmail.com>
2019-03-12 01:01:55 -0300

    agregado modulos BT

--- app/src/main/java/com/example/cristian/appbluetooth/MainActivity.java
+++ app/src/main/java/com/example/cristian/appbluetooth/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.cristian.appbluetooth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import static com.example.cristian.appbluetooth.R.layout.activity_main;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button conectarBT;
+    Button letfButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(activity_main);
+        setContentView(R.layout.activity_principal);
+
+        //enlazar los controles con sus respectivos views
+        conectarBT = (Button) findViewById(R.id.buttonBT);
+
     }
 }
